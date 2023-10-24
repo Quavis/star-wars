c866581cc20e3d2a7a4787ed67cc36fb13919f85
Quavis <56520608+Quavis@users.noreply.github.com>
2023-10-24 07:28:03 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@ import './styles/index.css';
 import './styles/App.css';
 import registerServiceWorker from './registerServiceWorker';
 //import React Router
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {BrowserRouter,
+        Routes,
+        Route }
+  from 'react-router-dom';
 //import Components
 import Explorer from './components/Explorer';
 import People from './components/People';
@@ -17,14 +20,12 @@ import BaseLayout from './components/Layout';
 ReactDOM.render(
   <BrowserRouter>
     <BaseLayout>
-      <Switch>
         <Route exact path="/" component={Explorer}/>
         <Route path="/starships" component={Starships}/>
         <Route path="/films" component={Films}/>
         <Route path="/people" component={People}/>
         <Route path="/details/:endpoint/:id" component={Details}/>
-      </Switch>
-    </BaseLayout>
+  </BaseLayout>
   </BrowserRouter>
 
 ,document.getElementById('root'));
